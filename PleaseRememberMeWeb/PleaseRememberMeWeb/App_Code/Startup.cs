77d98b982aa56a626149ed5fd5bf2ead2a15d46f
Marcos Bremont <bremont123@hotmail.com>
2022-04-26 11:09:38 -0400

    .

---
+++ PleaseRememberMeWeb/PleaseRememberMeWeb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PleaseRememberMeWeb.Startup))]
+namespace PleaseRememberMeWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
